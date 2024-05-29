a3d228db8b859ab84b23c90ba554a2fb9bcab7b1
yarden6 <yarden7@post.bgu.ac.il>
2024-05-29 15:49:09 +0300

    Update ReportFacade

--- dev/domain/ReportFacade.java
+++ dev/domain/ReportFacade.java
@@ -1,4 +1,9 @@
 package domain;
 
+import java.util.Dictionary;
+
 public class ReportFacade {
+    private CategoryFacade categoryFacade;
+    private Dictionary<Integer, Report> inventoryReports;
+    private Dictionary<Integer, Report> defectiveReports;
 }
