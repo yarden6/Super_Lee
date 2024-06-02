c9f941353bda727fe132add7c31f04fa851aae01
yarden6 <yarden7@post.bgu.ac.il>
2024-06-02 18:10:51 +0300

    Updates

--- dev/domain/InventoryReportByCategory.java
+++ dev/domain/InventoryReportByCategory.java
@@ -9,31 +9,33 @@ import java.util.List;
 
 public class InventoryReportByCategory extends Report {
 
-    public InventoryReportByCategory(int reportCounter, Hashtable<String,Category> categories, List<String> selectedCategories) {
+    public InventoryReportByCategory(int reportCounter, Hashtable<String,Category> categories, String[] selectedCategories) {
         super(reportCounter);
         createReport(categories , selectedCategories);
     }
 
 
-    //TODO TRY TO MAKE EFFIECIENT
-    public void createReport(Hashtable<String,Category> categories, List<String> selectedCategories){
-        if (selectedCategories.isEmpty()){
-            chooseCategory(categories,categories.keySet().stream().toList());
+    public void createReport(Hashtable<String,Category> categories, String[] selectedCategories){
+        if (selectedCategories.length == 0){ // if there are no selected categories, print all the categories
+            chooseCategory(categories,categories.keySet().toArray(new String[0]));
         }
         else
             chooseCategory(categories,selectedCategories);
     }
 
-    public void chooseCategory(Hashtable<String,Category> categories, List<String> selectedCategories) {
+    public void chooseCategory(Hashtable<String,Category> categories, String[] selectedCategories) {
         for (String name : selectedCategories) {
             Category selectedParent = categories.get(name);//for the main category
             selectedParent.toString();
-            for (Category sub : selectedParent.getSubCategories()) { //for the sub category
+            for (String s : selectedParent.getSubCategories().keySet()) { //for the sub category
+                Category sub = selectedParent.getSubCategories().get(s);
                 sub.toString();
-                for(Category subSub : sub.getSubCategories()){ //for the sub-sub category
+                for(String subSubName : sub.getSubCategories().keySet()){ //for the sub-sub category
+                    Category subSub = sub.getSubCategories().get(subSubName);
                     subSub.toString();
-                    for (Product p : subSub.getProducts()) { //for the products
-                        toString();
+                    for (Integer mkt : subSub.getProducts().keySet()) { //for the products
+                        Product p = subSub.getProducts().get(mkt);
+                        p.toString();
                     }
                 }
 
--- dev/domain/ReportFacade.java
+++ dev/domain/ReportFacade.java
@@ -11,33 +11,34 @@ public class ReportFacade {
     private Hashtable<Integer, Report> defectiveReports;
     private int reportCounter;
 
-    public ReportFacade(CategoryFacade cf){
-        this.categoryFacade=cf;
-        inventoryReports = new Hashtable<>() ;
+    public ReportFacade(CategoryFacade cf) {
+        this.categoryFacade = cf;
+        inventoryReports = new Hashtable<>();
         defectiveReports = new Hashtable<>();
         reportCounter = 1;
     }
 
-    public void makeInventoryReport(List<String> selectedCategories){
-        Report r = new InventoryReportByCategory(reportCounter,categoryFacade.getCategories(),selectedCategories);
-        inventoryReports.put(reportCounter,r);
+    public void makeInventoryReport(String[] selectedCategories) {
+        Report r = new InventoryReportByCategory(reportCounter, categoryFacade.getCategories(), selectedCategories);
+        inventoryReports.put(reportCounter, r);
+        reportCounter++;
 
     }
 
-    public void makeDefectiveReport(){
-        Report r = new DefectiveReport(reportCounter,categoryFacade.getCategories().get("Defective").getProducts());
-        defectiveReports.put(reportCounter,r);
+    public void makeDefectiveReport() {
+        Report r = new DefectiveReport(reportCounter, categoryFacade.getCategories().get("Defective").getProducts());
+        defectiveReports.put(reportCounter, r);
+        reportCounter++;
     }
 
-    public void publishReportWeekly(){
+    public void publishReportWeekly() {
         LocalDate current = LocalDate.now();
-        if (current.getDayOfWeek() == DayOfWeek.SUNDAY){
-            makeInventoryReport(new ArrayList<>());
+        if (current.getDayOfWeek() == DayOfWeek.SUNDAY) {
+            makeInventoryReport(new String[0]);
             makeDefectiveReport();
 
         }
     }
-
 
 
 }
